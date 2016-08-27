703f3501223cb3722200b1d0323a803d84018d44
yag_ays <yanagi.ayase@gmail.com>
2016-08-27 14:20:10 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pybitflyer",
+    version="0.0.1",
+    description="Python wrapper for Bitflyer's REST API.",
+    author="yag_ays",
+    author_email="yanagi.ayase@gmail.com",
+    url="https://github.com/yagays/pybitflyer",
+    install_requires=['requests']
+)
