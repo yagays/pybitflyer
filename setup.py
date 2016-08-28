5abf1eeeb18d5c530204bf12314b6f5c529d72b6
yag_ays <yanagi.ayase@gmail.com>
2016-08-28 21:45:41 +0900

    v0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name="pybitflyer",
     packages=['pybitflyer'],
-    version="0.0.1",
+    version="0.1.0",
     description="Python wrapper for Bitflyer's REST API.",
     author="yag_ays",
     author_email="yanagi.ayase@gmail.com",
